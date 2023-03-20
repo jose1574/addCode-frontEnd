813a2e98e043f3d333953d76f4e96f84994eb131
jose1574 <89107861+jose1574@users.noreply.github.com>
2023-03-19 22:59:28 -0400

    primer commit, se configura el formulario de conexion a la base de datos

---
+++ addCode/src/app/connection-data/components/connection-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-data',
+  templateUrl: './connection-data.component.html',
+  styleUrls: ['./connection-data.component.css']
+})
+export class ConnectionDataComponent {
+
+}
---
+++ addCode/src/app/connection-data/connection-data.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConnectionDataComponent } from './components/connection-data.component';
+
+@NgModule({
+  declarations: [ConnectionDataComponent],
+  imports: [CommonModule],
+})
+export class ConnectionDataModule {}
